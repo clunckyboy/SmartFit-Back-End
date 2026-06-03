777f0d41fc9ef43161ff46980beaeac3fe03932c
clunckyboy <edwinjonatanpurba@gmail.com>
2026-06-03 14:04:54 +0700

    fixed deploy preparations

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
-import server from './server/index.js';
+import app from './server/index.js';
 
-const port = process.env.PORT || 3000;
-// const host = process.env.HOST;
+if (process.env.NODE_ENV !== 'production') {
+  const port = process.env.port || 3000;
 
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-})
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
+
+export default app;
