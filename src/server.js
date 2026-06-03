ecbec563a41a2ce434b50f52afe0d57c4336f58c
clunckyboy <edwinjonatanpurba@gmail.com>
2026-06-03 12:53:01 +0700

    added prep to deploy

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import server from './server/index.js';
 
-const port = process.env.PORT;
-const host = process.env.HOST;
+const port = process.env.PORT || 3000;
+// const host = process.env.HOST;
 
 server.listen(port, () => {
-  console.log(`Server running at http://${host}:${port}`);
+  console.log(`Server running on port ${port}`);
 })
--- src/services/progress/db.js
+++ src/services/progress/db.js
@@ -2,11 +2,13 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
+  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:12345678@localhost:5432/smartfit',
+  // host: process.env.PGHOST,
+  // port: process.env.PGPORT,
+  // database: process.env.PGDATABASE,
+  // user: process.env.PGUSER,
+  // password: process.env.PGPASSWORD,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 export default pool;
